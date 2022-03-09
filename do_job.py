18996249c1d58ddd543df086744dfb4294160a8f
Brandon-M-Lee <minjae.py@gmail.com>
2022-03-09 23:59:14 +0900

    session으로 안되서 selenium을 씁시다

---
+++ do_job.py
@@ -0,0 +1,22 @@
+from lib2to3.pgen2 import driver
+import schedule
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from tool import get_links, make_driver
+import time
+
+driver = make_driver()
+
+def job():
+    links = get_links()
+    for link in links:
+        driver.get(link)
+        if not driver.find_element(By.CLASS_NAME, 'ytp-ad-text'):
+            time_duration = driver.find_element(By.CLASS_NAME, 'ytp-time-duration').text
+            minuite, second = map(int, time_duration.split(':'))
+            length = 60*minuite+second
+            time.sleep(length+1)
+        else:
+            
+
+job()
--- tool.py
+++ tool.py
@@ -1,20 +1,50 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+import pyperclip
 
-url = 'https://kndl.kr/kndl/requests'
-data = {
-    'id':'kndl',
-    'pw':'kndl',
-    'login_maintain':0
-}
+def make_driver():
+    chrome_path = "chromedriver.exe"
+    driver = webdriver.Chrome(chrome_path)
+    return driver
 
-session = requests.session()
-response = session.post(url, data=data)
+def get_links():
+    url_login = "https://kndl.kr/user/login"
+    url_requests = "https://kndl.kr/kndl/requests"
 
-response.raise_for_status()
+    driver = make_driver()
+    driver.get(url_login)
+    time.sleep(1)
 
-bsObject = BeautifulSoup(response.text, 'html.parser')
+    tag_id = driver.find_element(By.ID, 'id-input')
+    tag_pw = driver.find_element(By.ID, 'pw-input')
 
-for link in bsObject.find_all('a'):
-    print(link.test.strip(), link.get('href'))
+    tag_id.clear()
+    tag_id.click()
+    pyperclip.copy('kndl')
+    tag_id.send_keys(Keys.CONTROL, 'v')
+    tag_id.submit()
+    time.sleep(1)
+
+    tag_pw.clear()
+    tag_pw.click()
+    pyperclip.copy('kndl')
+    tag_pw.send_keys(Keys.CONTROL, 'v')
+    tag_pw.submit()
+    time.sleep(1)
+
+    maintain_btns = driver.find_elements(By.CLASS_NAME, 'choice-btn')
+    maintain_btns[1].click()
+    time.sleep(3)
+
+    driver.get(url_requests)
+    links = list()
+    for link in driver.find_elements(By.TAG_NAME, 'a'):
+        if link.get_attribute('href')[8:13] == 'youtu':
+            links.append(link.get_attribute('href'))
+
+    return links
+
+if __name__ == '__main__':
+    print(get_links())
