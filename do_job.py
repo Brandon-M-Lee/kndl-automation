90e6c81400f6680c6552979fd28359164143fc9d
Brandon-M-Lee <minjae.py@gmail.com>
2022-03-11 13:23:04 +0900

    이상한거 수정

--- do_job.py
+++ do_job.py
@@ -1,27 +1,24 @@
-from lib2to3.pgen2 import driver
-from platformdirs import site_config_dir
 import schedule
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from tool import get_links, make_driver
 import time
 
-driver = make_driver()
-
 def job():
+    driver = make_driver()
     links = get_links()
     for link in links:
         driver.get(link)
         if not driver.find_element(By.CLASS_NAME, 'ytp-ad-text'): # 광고 없을 떄
             time_duration = driver.find_element(By.CLASS_NAME, 'ytp-time-duration').text
             minuite, second = map(int, time_duration.split(':'))
-            length = 60*minuite+site_config_dir
+            length = 60*minuite+second
             time.sleep(length+1)
         else:
             pass
+    print(links)
 
-schedule.every().day.at("06:18").do(job)
+schedule.every().day.at("13:22").do(job)
 
 while True:
     schedule.run_pending()
-    time.sleep()
+    time.sleep(1)
