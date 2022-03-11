9e050241f0f45b1a0f75234efcefc3055fbf144c
Brandon-M-Lee <minjae.py@gmail.com>
2022-03-12 02:06:49 +0900

    노래는 올 랜덤, 노래가 부족하면 유튜브 탑 100에서 가져오기

--- do_job.py
+++ do_job.py
@@ -1,30 +1,6 @@
 import schedule
-from selenium.webdriver.common.by import By
-from tool import get_links, make_driver
+from tool import job
 import time
-import random
-
-def job():
-    driver = make_driver()
-    links = get_links()
-    links_to_play = random.sample(links, 3)
-    for link in links_to_play:
-        driver.get(link)
-        if not driver.find_element(By.CLASS_NAME, 'ytp-ad-text'): # 광고 없을 떄
-            time_duration = driver.find_element(By.CLASS_NAME, 'ytp-time-duration').text
-            minuite, second = map(int, time_duration.split(':'))
-            length = 60*minuite+second
-            time.sleep(length+1)
-        else:
-            while True:
-                if driver.find_elements(By.ID, 'ad-text:6'):
-                    ad_skip_btn = driver.find_element(By.ID, 'ad-text:6')
-                    ad_skip_btn.click()
-                    time_duration = driver.find_element(By.CLASS_NAME, 'ytp-time-duration').text
-                    minuite, second = map(int, time_duration.split(':'))
-                    length = 60*minuite+second
-                    time.sleep(length+1)
-                time.sleep(1)
 
 schedule.every().monday.at("06:18").do(job)
 schedule.every().tuesday.at("06:18").do(job)
--- tool.py
+++ tool.py
@@ -3,13 +3,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 import pyperclip
+import random
 
 def make_driver():
     chrome_path = "chromedriver.exe"
     driver = webdriver.Chrome(chrome_path)
     return driver
 
-def get_links():
+def get_request_links():
     url_login = "https://kndl.kr/user/login"
     url_requests = "https://kndl.kr/kndl/requests"
 
@@ -46,5 +47,40 @@ def get_links():
 
     return links
 
+def get_top_links():
+    top_links = list()
+    driver = make_driver()
+    url = 'https://www.youtube.com/playlist?list=PL4fGSI1pDJn6jXS_Tv_N9B8Z0HTRVJE0m'
+    driver.get(url)
+    time.sleep(1)
+    for link in driver.find_elements(By.ID, 'video-title'):
+        top_links.append(link.get_attribute('href'))
+    return top_links
+
+def job():
+    driver = make_driver()
+    links = get_request_links()
+    if len(links) >= 3:
+        links_to_play = random.sample(links, 3)
+    else:
+        links += random.sample(get_top_links(), 3-len(links))
+    for link in links_to_play:
+        driver.get(link)
+        if not driver.find_element(By.CLASS_NAME, 'ytp-ad-text'): # 광고 없을 떄
+            time_duration = driver.find_element(By.CLASS_NAME, 'ytp-time-duration').text
+            minuite, second = map(int, time_duration.split(':'))
+            length = 60*minuite+second
+            time.sleep(length+1)
+        else:
+            while True:
+                if driver.find_elements(By.ID, 'ad-text:6'):
+                    ad_skip_btn = driver.find_element(By.ID, 'ad-text:6')
+                    ad_skip_btn.click()
+                    time_duration = driver.find_element(By.CLASS_NAME, 'ytp-time-duration').text
+                    minuite, second = map(int, time_duration.split(':'))
+                    length = 60*minuite+second
+                    time.sleep(length+1)
+                time.sleep(1)
+
 if __name__ == '__main__':
-    print(get_links())
+    print(get_top_links())
