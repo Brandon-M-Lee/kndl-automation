e7d54336848c877feb1a968b681de780a0d9d67d
Brandon-M-Lee <minjae.py@gmail.com>
2022-03-11 13:43:20 +0900

    do_job 완성 학교가서 테스트 해봐야지

--- do_job.py
+++ do_job.py
@@ -14,8 +14,15 @@ def job():
             length = 60*minuite+second
             time.sleep(length+1)
         else:
-            pass
-    print(links)
+            while True:
+                if driver.find_elements(By.ID, 'ad-text:6'):
+                    ad_skip_btn = driver.find_element(By.ID, 'ad-text:6')
+                    ad_skip_btn.click()
+                    time_duration = driver.find_element(By.CLASS_NAME, 'ytp-time-duration').text
+                    minuite, second = map(int, time_duration.split(':'))
+                    length = 60*minuite+second
+                    time.sleep(length+1)
+                time.sleep(1)
 
 schedule.every().day.at("13:22").do(job)
 
